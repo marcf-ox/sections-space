98148887b73d94d2edf3f1edf8deb1717cea3ced
marcf <46935253+marcf-ox@users.noreply.github.com>
2024-09-05 15:18:15 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup, find_packages
+wfrom setuptools import setup, find_packages
 
 setuptools.setup(
     name='sections-space',
     version="0.0.0",
     author='Marcf',
     license='MIT',
-    install_requires=['numpy>=1.24.2','scipy>=1.10.0','cfractions>=2.2.0'],
+    install_requires=['numpy>=1.23','scipy>=1.10.0','cfractions>=2.2.0','networkx>=2.6.2','matplotlib>=3.3.4'],
     python_requires=">=3.8.0",
     packages=find_packages(),
 )
