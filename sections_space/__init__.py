e36de0675ee4b9639a40ca3015baa050ade60530
marcf <46935253+marcf-ox@users.noreply.github.com>
2024-09-05 16:28:38 +0100

    Update __init__.py

--- sections_space/__init__.py
+++ sections_space/__init__.py
@@ -1,2 +1,6 @@
 from .main import compute_sections,nx_graph_to_quiver
-__version__="0.0.0"
+from .aux_fun import proj, inj, intersection,null_space,sum_subspaces,extract_basis,row_echelon,col_echelon,solve_triangular,flatten_zero
+from .aux_fun import Field,zeros_mat,eye_mat,Quiver, E_in_out,Quiver,Field,shift_vertices
+from .strongly_connected import generate_SG_graphs,SG_to_tree
+from .arboreal import arboreal_out,generate_acyclic
+from .acycli_red import acyclic_red
