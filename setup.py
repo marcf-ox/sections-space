821d149551251bf0a97e2883db9afe4d6d3d8b62
marcf <46935253+marcf-ox@users.noreply.github.com>
2024-09-05 14:50:43 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setuptools.setup(
     version=__version__,
     author='Marcf',
     license='MIT',
-    install_requires=['numpy>=1.24.2'],
+    install_requires=['numpy>=1.24.2','scipy>=1.10.0','cfractions>=2.2.0'],
     python_requires=">=3.8.0",
     packages=find_packages(),
 )
