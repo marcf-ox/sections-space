89afc9807f02629714329d35b09b20a7c38a94b8
marcf <46935253+marcf-ox@users.noreply.github.com>
2024-09-06 16:26:08 +0100

    Update __init__.py

--- sections_space/__init__.py
+++ sections_space/__init__.py
@@ -1,2 +1,2 @@
 from .main import compute_sections,nx_graph_to_quiver
-from .aux_fun import Field,zeros_mat,eye_mat,Quiver, E_in_out,Quiver,Field,flatten_zero
+from .aux_fun import Field,zeros_mat,eye_mat,Quiver, E_in_out,Quiver,Field,flatten_zero,zeros_mat,eye_mat
